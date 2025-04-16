7c04c8ea1a8f6e6774750c3ae3933614f01e3263
SalahJamal1 <salahabufarha1996@outlook.com>
2025-04-16 12:22:14 +0300

    Update SearchController.java

--- server/src/main/java/com/digisolfze/app/controller/SearchController.java
+++ server/src/main/java/com/digisolfze/app/controller/SearchController.java
@@ -14,6 +14,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RestController
 @RequestMapping("/api/v1/expedia")
 public class SearchController {
+
+    @GetMapping
+    public ResponseEntity<JsonNode> getPackages() throws JsonProcessingException {
+        String url = "https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=test&productType=Package&clientId=test";
+  
+        RestTemplate restTemplate = new RestTemplate();
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+
+            String response = restTemplate.getForObject(url, String.class);
+            JsonNode jsonNode = objectMapper.readTree(response);
+            return ResponseEntity.ok(jsonNode.get("offers").get("Package"));
+        } catch (HttpClientErrorException err) {
+            String error = "{\"error \": \"Too Many Requests \"}";
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body(objectMapper.readTree(error));
+        } catch (Exception err) {
+            String error = err.getMessage();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(objectMapper.readTree(error));
+        }
+    }
     @GetMapping("/search")
     public ResponseEntity<JsonNode> searchPackages(@RequestParam String originCity,
             @RequestParam String destinationCity) throws JsonProcessingException {
